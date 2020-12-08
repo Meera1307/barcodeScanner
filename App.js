575a6b9ae723cc2e8a3df88227706842200a7ac2
V <67364479+Meera1307@users.noreply.github.com>
2020-12-08 13:18:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import scanScreen from './screens/scanScreen';
+
+export default class App extends React.Component{
+ render(){
+   return(
+<scanScreen/>   
+  
+
+  
+   )
+}
+}
+
